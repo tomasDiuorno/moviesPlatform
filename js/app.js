2126a8073bfca1c7545f2f0f74c52f310c51a6d4
Tomas Diuorno <105467737+tomasDiuorno@users.noreply.github.com>
2024-10-12 18:05:32 -0300

    cambios en suscribe

--- js/app.js
+++ js/app.js
@@ -1,16 +1,15 @@
 let annualBtn = document.getElementById('annual-btn');
 let monthlyBtn = document.getElementById('monthly-btn');
-let discount = document.querySelectorAll('.discount-container');
 
 monthlyBtn.addEventListener('click', changeMonthlyPrice);
 annualBtn.addEventListener('click', changeAnnualPrice);
+
 
 function changeAnnualPrice(){
     let priceOne = document.getElementById('plan-price-1');
     let priceTwo = document.getElementById('plan-price-2');
     let priceThree = document.getElementById('plan-price-3');
-
-
+    
     monthlyBtn.style.color = "#8a8a8a"
     annualBtn.style.color = "#f5f5f5"
     priceOne.innerText = "ARS 35,990.00/mes"
@@ -22,10 +21,11 @@ function changeMonthlyPrice(){
     let priceOne = document.getElementById('plan-price-1');
     let priceTwo = document.getElementById('plan-price-2');
     let priceThree = document.getElementById('plan-price-3');
-    
+
     annualBtn.style.color = "#8a8a8a"
     monthlyBtn.style.color = "#f5f5f5"
     priceOne.innerText = "ARS 3,599.00/mes"
     priceTwo.innerText = "ARS 4,499.00/mes"
     priceThree.innerText = "ARS 5,399.00/mes"
+
 }
