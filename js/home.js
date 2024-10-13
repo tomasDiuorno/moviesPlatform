ddcf5f22a993e94324193fa1eff46e67fc48a693
Tomas Diuorno <105467737+tomasDiuorno@users.noreply.github.com>
2024-10-13 01:10:49 -0300

    actualizacion de carrusel

---
+++ js/home.js
@@ -0,0 +1,38 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+prevDom.addEventListener('click', function(){
+    showSlider('prev');
+})
+
+nextDom.addEventListener('click', function(){
+    showSlider('next');
+})
+
+let timeRunning = 3000;
+let runTimeOut;
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+    if(type === 'next'){
+        listItemDom.appendChild(itemSlider[0]);
+        thumbnailDom.appendChild(itemThumbnail[0]);
+        carouselDom.classList.add('next');
+    }else{
+        let positionLastItem = itemSlider.length - 1;
+        listItemDom.prepend(itemSlider[positionLastItem]);
+        thumbnailDom.prepend(itemThumbnail[positionLastItem]);
+        carouselDom.classList.add('prev');
+    }
+    
+    clearTimeout(runTimeOut);
+    runTimeOut = setTimeout(() => {
+        carouselDom.classList.remove('next');
+        carouselDom.classList.remove('prev');
+    }, timeRunning);
+}
