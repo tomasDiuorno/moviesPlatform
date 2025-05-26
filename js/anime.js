1b646e932af63bafb34ea5cd8bbb7af28617606e
Tomas Diuorno <tom.diuorno99@gmail.com>
2025-05-26 17:21:19 -0300

    comienzo del search

---
+++ js/anime.js
@@ -0,0 +1,29 @@
+const series = [
+    {id: 1, name: "Attack On Titan", description: "", image: "", logo: "", languaje: ""},
+    {id: 2, name: "One Piece", description: "", image: "", logo: "", languaje: ""},
+    {id: 3, name: "Chainsaw Man", description: "", image: "", logo: "", languaje: ""},
+    {id: 4, name: "Kimetsu no Yaiba", description: "", image: "", logo: "", languaje: ""},
+    {id: 5, name: "Naruto", description: "", image: "", logo: "", languaje: ""},
+    {id: 6, name: "Naruto Shippuden", description: "", image: "", logo: "", languaje: ""},
+    {id: 7, name: "Dragon Ball Super", description: "", image: "", logo: "", languaje: ""},
+    {id: 8, name: "Jujutsu Kaisen", description: "", image: "", logo: "", languaje: ""},
+    {id: 9, name: "Hunte x Hunter", description: "", image: "", logo: "", languaje: ""},
+    {id: 10, name: "Dan Da Dan", description: "", image: "", logo: "", languaje: ""},
+    {id: 11, name: "Solo Leveling", description: "", image: "", logo: "", languaje: ""},
+    {id: 12, name: "My Hero Academy", description: "", image: "", logo: "", languaje: ""},
+    {id: 13, name: "Fire Force", description: "", image: "", logo: "", languaje: ""},
+    {id: 14, name: "The Daily Life of The Inmortal King", description: "", image: "", logo: "", languaje: ""},
+    {id: 15, name: "The Beginning After the End", description: "", image: "", logo: "", languaje: ""},
+    {id: 16, name: "Spy x Family", description: "", image: "", logo: "", languaje: ""},
+    {id: 17, name: "Dr. Stone", description: "", image: "", logo: "", languaje: ""},
+    {id: 18, name: "Hell's Paradise", description: "", image: "", logo: "", languaje: ""},
+    {id: 19, name: "Jojo's", description: "", image: "", logo: "", languaje: ""},
+    {id: 20, name: "Black Clover", description: "", image: "", logo: "", languaje: ""},
+]
+
+function mostrarBusqueda(animes){
+    contenedorBusqueda.innerHTML = '';
+}
+
+
+mostrarBusqueda(series);
